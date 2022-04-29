f07dd3c075c718434cd46380f7bd21ce821fbbdc
Alex Li <7alex7li@gmail.com>
2022-04-29 05:07:08 -0400

    Add the backend files

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,33 +1,16 @@
 import express from "express";
 import * as functions from "firebase-functions";
-import {getDatabase, ref, set} from "firebase/database";
-import firebase from "../firebase";
 import cors from "cors";
 import {getForums, postForum} from "./routes/forum";
 import {getFigures, postFigure} from "./routes/figure";
 import {getOrders, postOrder} from "./routes/order";
-import {getUsers} from "./routes/user";
+import {getUsers, newUser} from "./routes/user";
 
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 const app = express();
 app.use(cors({origin: true}));
-
-app.get("/", (req, res) => {
-  res.send("hello worldy");
-});
-
-// Just an example of how to use the database
-// http://localhost:5000/claymore-d6749/us-central1/default/dog
-app.get("/dog", (req, res) => {
-  res.send("updating database");
-  functions.logger.info("Hello logs!", {structuredData: true});
-  const db = getDatabase(firebase);
-  set(ref(db, "users/1"), {
-    username: "alex4",
-  });
-});
 
 app.get("/figure", (req, res) => {
   getFigures(req, res);
@@ -53,8 +36,12 @@ app.post("/forum", (req, res) => {
   postForum(req, res);
 });
 
-app.post('/order', (req, res) => {
+app.post("/order", (req, res) => {
   postOrder(req, res);
 });
 
-export default functions.https.onRequest(app) ;
+app.post("/user", (req, res) => {
+  newUser(req, res);
+});
+
+export default functions.https.onRequest(app);
