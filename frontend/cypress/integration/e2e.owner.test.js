6d29eee4846ba1a14907e0eef4275324cb582556
Max Dunaevschi <maxdunaevschi@Maxs-MacBook-Air.local>
2022-05-01 15:27:02 -0400

    Fixed post request cancel in owner E2E

--- frontend/cypress/integration/e2e.owner.test.js
+++ frontend/cypress/integration/e2e.owner.test.js
@@ -16,24 +16,24 @@ describe('Owner page tests', function () {
 
     it('Adds a new figure to the shop correctly', () => {
        cy.get("[id=itemName]").type("Cypress test figure")
-       cy.get("[id=imageURL]").type("https://cypresstesturl")
+       cy.get("[id=imageURL]").type("https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png")
        cy.get("[id=stock]").type("10")
        cy.get("[id=price]").type("999.99")
        cy.get("Button").contains("Add Item").click()
        cy.contains("Item added! Check menu for update")
-       cy.visit('http://localhost:3000/addFigure')
+       cy.visit('http://localhost:3000/menu')
        cy.contains("Cypress test figure")
     })
 
     it('Adds a new figure to the shop correctly', () => {
         cy.get("[id=itemName]").type("Cypress test pre-order figure")
-        cy.get("[id=imageURL]").type("https://cypresstesturl")
+        cy.get("[id=imageURL]").type("https://user-images.githubusercontent.com/11304944/76085431-fd036480-5fec-11ea-8412-9e581425344a.png")
         cy.get("[id=stock]").type("10")
         cy.get("[id=price]").type("999.99")
         cy.get("[id=present]").click()
         cy.get("Button").contains("Add Item").click()
         cy.contains("Item added! Check menu for update")
-        cy.visit('http://localhost:3000/addFigure')
+        cy.visit('http://localhost:3000/menu')
         cy.contains("Cypress test pre-order figure")
      })
     
